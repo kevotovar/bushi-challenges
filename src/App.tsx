4b4f95ba0a2e3559fe0b662e91a869de3a556e33
Kevin Tovar <kevotovar14@gmail.com>
2022-07-30 11:07:57 -0500

    Upate shop dates

--- src/App.tsx
+++ src/App.tsx
@@ -14,23 +14,23 @@ import "react-big-calendar/lib/css/react-big-calendar.css";
 const dates = [
   {
     title: "Chop Chop Shop v premium",
-    start: moment("2022-08-26T17:00:00-05:00").toDate(),
-    end: moment("2022-08-26T20:00:00-05:00").toDate(),
+    start: moment("2022-09-16T17:00:00-05:00").toDate(),
+    end: moment("2022-09-16T20:00:00-05:00").toDate(),
   },
   {
     title: "Chop Chop Shop Premium",
-    start: moment("2022-09-03T17:00:00-05:00").toDate(),
-    end: moment("2022-09-03T20:00:00-05:00").toDate(),
+    start: moment("2022-09-02T17:00:00-05:00").toDate(),
+    end: moment("2022-09-02T20:00:00-05:00").toDate(),
   },
   {
     title: "Chop Chop Shop Overdress",
-    start: moment("2022-09-23T17:00:00-05:00").toDate(),
-    end: moment("2022-09-23T20:00:00-05:00").toDate(),
+    start: moment("2022-09-30T17:00:00-05:00").toDate(),
+    end: moment("2022-09-30T20:00:00-05:00").toDate(),
   },
   {
     title: "Chop Chop Shop Weiss Schwarz",
-    start: moment("2022-09-30T17:00:00-05:00").toDate(),
-    end: moment("2022-09-30T20:00:00-05:00").toDate(),
+    start: moment("2022-09-09T17:00:00-05:00").toDate(),
+    end: moment("2022-09-09T20:00:00-05:00").toDate(),
   },
   {
     title: "Estigia games v premium",
